de4491927ebb80ad377d5a2869433921453ac61f
Sujit-Panigrahi5 <panigrahisujit588@gmail.com>
2023-09-30 23:22:39 +0530

    add all project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./backgroundcolorChange/*.html","./nftlandingpage/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
